403057f91c977b0e28ae7e44c378438135574e85
Garth Bushell <garth@garthy.com>
2011-09-28 13:51:42 +0100

    add mising icon funcs for bluetooth

--- Watch/Application/Bluetooth.h
+++ Watch/Application/Bluetooth.h
@@ -2,7 +2,9 @@
 #define METWATCH_BLUETOOTH_WRAPPERS_H
 
 void bluetooth_toggle_bluetooth(void);
+unsigned char const * bluetooth_get_status_icon();
 void bluetooth_toggle_discoverability(void);
+unsigned char const * bluetooth_get_discoverability_icon(void);
 void bluetooth_toggle_secure_smiple_pairing(void);
 
 #endif /* METWATCH_BLUETOOTH_WRAPPERS_H */
